0a8168cba914c3c14fd799bb088a79a08d35e94b
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-02 05:03:32 +0200

    update

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -56,6 +56,9 @@ class Register extends Component {
 
           this.props.ElninonChangeRoute("home");
         }
+      })
+      .catch((err) => {
+        console.log("We couldnt register your user", err);
       });
   };
 
